3e1f74903de5f8f103882621d03ca8621658d5bb
Robert Murray <work@robertmurray.me>
2020-04-09 09:29:45 -0700

    minor improvements. Fixed bug w/ America.

--- pages/api/america.js
+++ pages/api/america.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { US_STATES_URL } from "../../lib/urls"; 
 
 async function handler(req, res) {
-  const response = await axios.get(US_STATES_URL); 
+  const response = await axios.get(US_URL); 
+  console.log("data: ", ...response.data); 
   res.json({ ...response.data });
 }
 
---
+++ pages/api/scripts/nyt_states.js
@@ -0,0 +1,39 @@
+import axios from "axios"; 
+import faunadb from "faunadb";
+import dotenv from "dotenv"; 
+
+import { NYT_STATES_CSV } from "../../../lib/urls"; 
+
+dotenv.config(); 
+
+async function handler(req, res) {
+
+  // const q = faunadb.query;
+  // const client = new faunadb.Client({ secret: process.env.FAUNADB_RONA_SECRET });
+  // // const dbs = await client.query(q.Paginate(q.Keys())); 
+  
+  // // const col = await client.query(q.Get(q.Collection('ny_times_us_states')));
+
+  let r = []
+
+  // const raw_csv = await axios.get(NYT_STATES_CSV); 
+  // const data = raw_csv.data.split("\n"); 
+  // // console.log("dbs: ", dbs); 
+  // // console.log("cols:", col);
+
+  // data.map(item => {
+  //   const [date, state, fips, cases, deaths] = item.split(","); 
+    
+  //   const data = { date, state, fips: parseInt(fips), cases: parseInt(cases), deaths: parseInt(deaths) };
+
+  //   client.query(
+  //     q.Create(q.Collection("ny_times_us_states"), {
+  //       data
+  //     })
+  //   );
+  //   r.push(data); 
+  // })
+  res.json(r);
+}
+
+export default handler;
